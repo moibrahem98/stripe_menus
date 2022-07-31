af4de508015c5a212eb5badb07ccf810a5dc033b
moibrahem98 <mo.ibrahiim98@gmail.com>
2022-08-01 01:41:24 +0300

    adding the file structure and submenus changes

---
+++ src/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import logo from './images/logo.svg'
+import { FaBars } from 'react-icons/fa'
+
+import { useGlobalContext } from './context'
+
+const Navbar = () => {
+  const { openSidebar, closeSubMenu, openSubMenu } = useGlobalContext();
+  return <nav className="nav">
+    <div className='nav-center'>
+      <div className='nav-header'>
+        <img src={logo} className='nav-logo' alt="stripe" />
+        <button onClick={openSidebar} className='btn toggle-btn'>
+          <FaBars />
+        </button>
+      </div>
+      <ul className='nav-links'>
+        <li><button className='link-btn'>Products</button></li>
+        <li><button className='link-btn'>Developers</button></li>
+        <li><button className='link-btn'>Company</button></li>
+      </ul >
+      <button className='btn signin-btn'>Sign In</button>
+    </div>
+  </nav>
+}
+
+export default Navbar
---
+++ src/context.js
@@ -0,0 +1,33 @@
+import React, { useState, useContext } from 'react'
+import sublinks from './data'
+
+const AppContext = React.createContext();
+
+
+export const AppProvider = (children) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+    const [isSubMenuOpen, setIsSubMenuOpen] = useState(true);
+
+    const openSidebar = () => {
+        setIsSidebarOpen(true)
+    }
+    const closeSidebar = () => {
+        setIsSidebarOpen(false)
+    }
+    const openSubMenu = () => {
+        setIsSubMenuOpen(true)
+    }
+    const closeSubMenu = () => {
+        setIsSubMenuOpen(false)
+    }
+
+
+
+    return <AppContext.Provider value={{ isSidebarOpen, isSubMenuOpen, openSidebar, closeSubMenu, openSubMenu, closeSidebar }}>
+        {children}
+    </AppContext.Provider>
+}
+
+
+export const useGlobalContext = () => { return useContext(AppContext) }
+
